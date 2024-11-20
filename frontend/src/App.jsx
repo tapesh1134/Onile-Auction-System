48d6b8aa75db08e9f6cd4ebe66c9ac406ac01f08
Tapesh Sharma <tapeshs1134@gmail.com>
2024-11-20 20:10:12 +0530

    fixing pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,4 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SideDrawer from "./layout/SideDrawer";
-import Home from "./pages/Home";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
-import SubmitCommission from "./pages/SubmitCommission";
-import { useDispatch } from "react-redux";
-import { fetchLeaderboard, fetchUser } from "./store/slices/userSlice";
-import HowItWorks from "./pages/HowItWorks";
-import About from "./pages/About";
-import { getAllAuctionItems } from "./store/slices/auctionSlice";
-import Leaderboard from "./pages/Leaderboard";
-import Auctions from "./pages/Auctions";
-import AuctionItem from "./pages/AuctionItem";
-import CreateAuction from "./pages/CreateAuction";
-import ViewMyAuctions from "./pages/ViewMyAuctions";
-import ViewAuctionDetails from "./pages/ViewAuctionDetails";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Contact from "./pages/Contact";
-import UserProfile from "./pages/UserProfile";
 
 const App = () => {
   const dispatch = useDispatch();
